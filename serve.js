febf055796500312d5a5274d145e0c48f33f1287
Mattias Petter Johansson <mpj@mpj.me>
2017-08-18 17:47:29 +0200

    added response

--- serve.js
+++ serve.js
@@ -7,8 +7,9 @@ function serve({ express, bodyParser }) {
   
   app.post('/webhook', (req, res) => {
     console.log('req.body', req.body)
+    res.status(200).send('ok')
   })
-  
+
   app.get('/', (req,res) => {
     res.send('k')
   })
