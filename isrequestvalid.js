488330ad08e76bd9bf8be6e1282bb59e6be8a8d8
Mattias Petter Johansson <mpj@mpj.me>
2017-08-18 18:30:33 +0200

    added missing file

---
+++ isrequestvalid.js
@@ -0,0 +1,7 @@
+function isRequestValid({ crypto, webhookSecret }, request) {
+  let hmac = crypto.createHmac('sha256', webhookSecret)
+  let hash = `sha256=${hmac.update(rawBody).digest('hex')}`
+  return hash === request.headers['x-discourse-event-signature']
+}
+
+module.exports = isRequestValid
