9802b2d4fe5140958569b838b94d8606c3c33791
Mattias Petter Johansson <mpj@mpj.me>
2017-08-28 00:55:43 +0200

    remove logging

--- isrequestvalid.js
+++ isrequestvalid.js
@@ -1,5 +1,4 @@
 function isRequestValid({ crypto, webhookSecret }, request) {
-  console.log('rawBody is', rawBody)
   let hmac = crypto.createHmac('sha256', webhookSecret)
   let hash = `sha256=${hmac.update(request.rawBody).digest('hex')}`
   return hash === request.headers['x-discourse-event-signature']
