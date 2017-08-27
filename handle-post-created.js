f9c5aa658103532bfb9c5e4f64ab0391e50d06a8
Mattias Petter Johansson <mpj@mpj.me>
2017-08-27 21:58:40 +0200

    inline debugging statement

--- handle-post-created.js
+++ handle-post-created.js
@@ -8,12 +8,10 @@ let makeHandlePostCreated = ({ assignBadge }) => ({
     return Promise.resolve()
 
   let INTRODUCTION_BADGE_ID = 104
-  let opts = {
+  return assignBadge({
     username,
     badgeId: INTRODUCTION_BADGE_ID,
     reason: `https://www.funfunforum.com/t/${topicId}/${postNumber}`
-  }
-  console.log('opts', opts)
-  return assignBadge(opts)
+  })
 }
 module.exports = makeHandlePostCreated
