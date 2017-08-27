c631b2755bcf7146c1bfe857a16cbf4d21bb02d5
Mattias Petter Johansson <mpj@mpj.me>
2017-08-28 00:18:03 +0200

    another attempt

--- serve.js
+++ serve.js
@@ -19,18 +19,11 @@ function serve({
   app.use(cors())
 
   // Reimplement rawBody
-  app.use(function(req, res, next) {
-    req.rawBody = ''
-    req.setEncoding('utf8')
-  
-    req.on('data', function(chunk) { 
-      req.rawBody += chunk
-    })
-  
-    req.on('end', function() {
-      next()
-    })
-  })
+  app.use (function(req, res, next) {
+    req.rawBody = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) { req.rawBody += chunk });
+  });
   
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: false }))
