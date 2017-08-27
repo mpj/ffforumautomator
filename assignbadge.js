adcb78981355f14a9a19bedfe9b4eccbec064d1d
Mattias Petter Johansson <mpj@mpj.me>
2017-08-28 00:12:35 +0200

    simples syntax

--- assignbadge.js
+++ assignbadge.js
@@ -1,19 +1,14 @@
-let R = require('ramda')
 let errorUnlessOK = require('./error-unless-ok')
 
-const makeAssignBadge = ({ postAsJSONTo, discourseAPIUrl }) => {
-  const 
-    makeBody = ({ username, badgeId, reason }) => Promise.resolve({
+const makeAssignBadge = 
+  ({ postAsJSONTo, discourseAPIUrl }) => 
+  ({ username, badgeId, reason }) =>
+    Promise.resolve({
       username,
       badge_id: badgeId,
       reason
     })
-
-  return R.pipeP(
-    makeBody,
-    postAsJSONTo(discourseAPIUrl('/user_badges.json')),
-    errorUnlessOK('assignBadge')
-  )
-}
+    .then(postAsJSONTo(discourseAPIUrl('/user_badges.json')))
+    .then(errorUnlessOK)
 
 module.exports = makeAssignBadge
