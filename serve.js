a61d686637d774de96d2cf2c399249cde8b4d46e
Mattias Petter Johansson <mpj@mpj.me>
2017-08-28 00:47:53 +0200

    remove it again

--- serve.js
+++ serve.js
@@ -18,19 +18,12 @@ function serve({
 
   app.use(cors())
 
-  // Reimplement rawBody
-  app.use (function(req, res, next) {
-    req.rawBody = '';
-    req.setEncoding('utf8');
-    req.on('data', function(chunk) { req.rawBody += chunk });
-  });
-  
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: false }))
 
   app.post('/webhook', wrap(async function(req, res) {
-    if (!isRequestValid(req))
-      res.status(403).send('invalid signature')
+    //if (!isRequestValid(req))
+      //res.status(403).send('invalid signature')
 
     if(req.headers['x-discourse-event'] === 'user_created') {
       let { username, created_at } = req.body.user
