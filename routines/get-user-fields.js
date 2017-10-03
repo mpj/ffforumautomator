9270b1ea5669a9680ffeea8886817776b4413934
Mattias Petter Johansson <mpj@mpj.me>
2017-10-03 16:15:59 +0200

    remove commented code

--- routines/get-user-fields.js
+++ routines/get-user-fields.js
@@ -15,21 +15,3 @@ module.exports = bus => {
     })
     .onCallback('fetch-response-json', R.prop('user_fields'))
 }
-
-
-/*
-let parseAsJSON = require('./parse-as-json')
-const makeGetUserFields = ({ bus, fetch }) => {
-  const promised = fn => R.pipe(fn, x => Promise.resolve(x))
-  const extractFields = R.prop('user_fields')
-
-  return R.pipeP(
-    () => bus.call('discourse-api-url', { path: `/admin/customize/user_fields.json` }),
-    fetch,
-    errorUnlessOK('getUserFields'),
-    parseAsJSON,
-    promised(extractFields)
-  )
-}
-
-module.exports = makeGetUserFields*/
