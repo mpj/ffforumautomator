49d3e42721ea3ddaee99534bade025ad771ebe3c
Mattias Petter Johansson <mpj@mpj.me>
2017-12-19 11:39:37 +0100

    add dirty rate limiting hack :(

--- routines/fetch.js
+++ routines/fetch.js
@@ -1,17 +1,41 @@
 let fetch = require('node-fetch')
 let uuid = require('uuid/v4')
 
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
 module.exports = bus => {
   const responses = {}
 
-  bus.impure.onCall('fetch', ({ url, opts }) => fetch(url, opts).then(response => {
-    const id = uuid()
-    responses[id] = response
-    return {
-      id,
-      status: response.status
-    }
-  }))
+  const standardFetch = (url, opts) =>
+    fetch(url, opts)
+      .then(response => {
+        if (response.status === 429) {
+          return {
+            rateLimited: true,
+          }
+        }
+        const id = uuid()
+        responses[id] = response
+        return {
+          id,
+          rateLimited: false,
+          status: response.status
+        }
+      })
+
+  bus.impure.onCall('fetch', ({ url, opts }) => {
+    let backoff = 1000
+
+    return standardFetch(url, opts)
+      .then(response => {
+        if (response.rateLimited) {
+          backoff = backoff * 2
+          return wait(backoff)
+            .then(() => standardFetch(url, opts))
+        }
+        return response
+      })
+  })
 
   bus.impure.onCall('fetch-response-json', id => {
     let res = responses[id].json()
