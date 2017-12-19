bdf3a76304a2521ca0d78ce4116ec3febad007ce
Mattias Petter Johansson <mpj@mpj.me>
2017-12-19 11:46:57 +0100

    use fetch routine

--- getuserbyusername.js
+++ getuserbyusername.js
@@ -1,12 +1,12 @@
 function getUserByUsername({ fetch, bus }, username) {
 
   return bus.call('discourse-api-url', { path: `/users/${username}.json` })
-    .then(fetch)
-    .then(response => {
-      if (response.status !== 200) {
-        throw new Error(`list endpoint returned non-200 status: ${response.status}`)
+    .then(url => bus.call('fetch', { url }))
+    .then(({ status, id }) => {
+      if (status !== 200) {
+        throw new Error(`list endpoint returned non-200 status: ${status}`)
       }
-      return response.json()
+      return bus.call('fetch-response-json', { id })
     })
 }
 
